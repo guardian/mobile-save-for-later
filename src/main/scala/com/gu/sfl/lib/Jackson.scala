3ad4ec07877b40c6e39c52bbe48b1477830bfb4c
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2018-04-10 10:44:38 +0100

    fix domain name, add date to mapper

--- src/main/scala/com/gu/sfl/lib/Jackson.scala
+++ src/main/scala/com/gu/sfl/lib/Jackson.scala
@@ -1,10 +1,18 @@
 package com.gu.sfl.lib
 
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+
 import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 object Jackson {
+
+  val formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+
   val mapper = new ObjectMapper
+
   mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  mapper.setDateFormat(formatter)
   mapper.registerModule(DefaultScalaModule)
 }
