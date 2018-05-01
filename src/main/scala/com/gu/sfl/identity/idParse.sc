1c27ded9733b743e57ca14da04be8deb12d543d4
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2018-05-01 15:57:07 +0100

    promises and fangles

--- src/main/scala/com/gu/sfl/identity/idParse.sc
+++ src/main/scala/com/gu/sfl/identity/idParse.sc
@@ -1,9 +1,12 @@
+import com.gu.sfl.lambda.LambdaResponse
+
 import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.gu.sfl.lib.Jackson._
+import scala.concurrent.duration._
 
 import scala.concurrent.duration.Duration
-import scala.util.{Success, Try}
+import scala.util.{Failure, Success, Try}
 
 case class Saved(id: String, arts: List[String] = List.empty)
 case class Response(value: String)
@@ -16,23 +19,33 @@ val saved = Saved("1234", List("buy-conservatory", "kill-corbyn", "global-warmin
 val r = Response("1234")
 
 def good(id: String) : Future[Option[Saved]] = {
+  //Thread.sleep(1000)
   Future{ Some(saved) }
 }
 
-def f: Future[String] = {
-  Thread.sleep(4300)
-  Future {"Hello"}
+val futureRes = good("1234").transformWith {
+  case Success(Some(s)) => Future{ Response(mapper.writeValueAsString(s)) }
+  case Success(None) => Future { Response("SoMuchBlather")}
+  case Failure(_) => Future { Response("Utter Bs") }
 }
 
-val h = Await.result(f, Duration.Inf)
-println(s"H: $h")
-println("Wotcha!")
+futureRes.transform((triedResponse: Try[Response]) => {
+  promiseResponse.complete(triedResponse)
+  triedResponse
+})
 
 
-/*good("1234").transform((trySaved: Try[Option[Saved]]) => {
+Await.ready(futureResponse, Duration(4, SECONDS))
 
+val end = futureResponse.value.getOrElse(Failure(new IllegalStateException("Mutha!")))
+
+end match {
+  case Success(response) => response
+  case Failure(t) => {
+     println("bibbledu-bob")
+    Response("Yosa")
+  }
 }
-)*/
 
 
 
--- src/main/scala/com/gu/sfl/lambda/LambdaApiGateway.scala
+++ src/main/scala/com/gu/sfl/lambda/LambdaApiGateway.scala
@@ -10,9 +10,9 @@ import com.gu.sfl.util.StatusCodes
 import org.apache.commons.io.IOUtils
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-
+import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success, Try}
 
 object ApiGatewayLambdaResponse extends Base64Utils {
   def apply(lamdaResponse: LambdaResponse): ApiGatewayLambdaResponse = ApiGatewayLambdaResponse(lamdaResponse.statusCode, None, lamdaResponse.headers)
@@ -110,7 +110,53 @@ class LambdaApiGatewayImpl(function: (LambdaRequest => Future[LambdaResponse])) 
     }
   }
 
+  //TODO shit name change it
+  private def marshallResult(apiGatewayLambdaRequest: ApiGatewayLambdaRequest) : ApiGatewayLambdaResponse = {
+
+      logger.info("Making lambda request")
+
+      val promiseLambdaResponse: Promise[LambdaResponse] = Promise[LambdaResponse]
+      val futureLambdaResponse: Future[LambdaResponse] = promiseLambdaResponse.future
+
+      function(LambdaRequest(apiGatewayLambdaRequest)).transform((triedLambdaResponse: Try[LambdaResponse]) => {
+        promiseLambdaResponse.complete(triedLambdaResponse)
+        triedLambdaResponse
+      })
+
+      Await.ready(futureLambdaResponse, Duration(1500, MILLISECONDS))
+
+      futureLambdaResponse.value.getOrElse(Failure(new IllegalStateException("Bark! Bark"))) match {
+        case Success(lambdaResponse) =>
+          logger.info(s"Got Result: $lambdaResponse")
+          ApiGatewayLambdaResponse(lambdaResponse)
+        case Failure(t) =>
+          logger.info(s"lambda returned error")
+          ApiGatewayLambdaResponse(StatusCodes.internalServerError)
+      }
+
+  }
+
   override def execute(inputStream: InputStream, outputStream: OutputStream): Unit = {
+      logger.info("ApiGateway: Execute")
+
+      try {
+        mapper.writeValue(outputStream, objectReadAndClose(inputStream) match {
+          case Left(apiGatewayLambdaRequest) =>
+            logger.info("Read request OK")
+            marshallResult(apiGatewayLambdaRequest)
+          case Right(_) =>
+            logger.info("Error reading request")
+            ApiGatewayLambdaResponse(StatusCodes.internalServerError)
+        })
+        
+      } finally {
+        logger.info("ApiGateway: closing output")
+        outputStream.close()
+      }
+
+  }
+
+  def XXXexecute(inputStream: InputStream, outputStream: OutputStream): Unit = {
     logger.info("ApiGateway: Execute")
     try {
       val response: Future[ApiGatewayLambdaResponse] = objectReadAndClose(inputStream) match {
