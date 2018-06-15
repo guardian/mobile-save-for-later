85555cd6b22f71fbd3b4b66ff4f8eb9afb39f15e
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-06-15 17:00:02 +0100

    With Nathaniel:  - pair to review the merge logic

--- src/main/scala/com/gu/sfl/lib/SavedArticlesMerger.scala
+++ src/main/scala/com/gu/sfl/lib/SavedArticlesMerger.scala
@@ -30,39 +30,22 @@ class SavedArticlesMergerImpl(savedArticlesMergerConfig: SavedArticlesMergerConf
 
   override def updateWithRetryAndMerge(userId: String, savedArticles: SavedArticles): Try[Option[SyncedPrefs]] = {
 
-    @tailrec
-    def loop(articles: SavedArticles, retries: Int): Try[Option[SyncedPrefs]] = {
-
-      if(retries == 0) {
-        logger.info(s"Failed to merge saved articles for user: $userId")
-        Failure(SavedArticleMergeError("Conflicting version in savedArticles") )
-      } else {
-        savedArticlesPersistence.read(userId) match {
-          case Success(Some(currentArticles)) =>
-            val mergedArticles = Merge.mergeListBy(currentArticles.articles, articles.articles)(_.id)
-            if(currentArticles.version == articles.version) {
-               logger.debug(s"Merging new articles list for user: $userId")
-               persistMergedArticles(userId, SavedArticles(articles.version, mergedArticles))(persistOperation = savedArticlesPersistence.update)
-             }
-             else {
-               logger.info(s"Conflicting merge try on saving articles. trying again")
-               val nextTryArticles = currentArticles.copy(articles = mergedArticles)
-               loop(nextTryArticles, retries - 1)
-             }
-
-          case Success(None) =>
-            logger.info("Adding articles for new user")
-             persistMergedArticles(userId, articles)(savedArticlesPersistence.write)
-          case _ => Failure(SavedArticleMergeError("Could not retrieve current articles"))
-        }
-      }
-    }
-
     if( savedArticles.articles.lengthCompare(maxSavedArticlesLimit) > 0 ){
       logger.info(s"User $userId tried to save ${savedArticles.articles.length} articles. Limit is ${maxSavedArticlesLimit}.")
       Failure(MaxSavedArticleTransgressionError(s"Tried to save more than $maxSavedArticlesLimit articles.") )
     } else {
-      loop(savedArticles, 3)
+      savedArticlesPersistence.read(userId) match {
+        case Success(Some(currentArticles)) if currentArticles.version == savedArticles.version =>
+          persistMergedArticles(userId, savedArticles)(savedArticlesPersistence.update)
+        case Success(Some(currentArticles)) =>
+          val articlesToSave = savedArticles.copy(articles = Merge.mergeListBy(currentArticles.articles, savedArticles.articles)(_.id))
+          persistMergedArticles(userId, articlesToSave)(savedArticlesPersistence.update)
+        case Success(None) =>
+          logger.info("Adding articles for new user")
+          persistMergedArticles(userId, savedArticles)(savedArticlesPersistence.write)
+        case _ => Failure(SavedArticleMergeError("Could not retrieve current articles"))
+      }
+
     }
   }
 }
