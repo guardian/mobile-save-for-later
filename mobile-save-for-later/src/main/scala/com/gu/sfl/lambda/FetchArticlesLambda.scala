388a5437bb44d8f878288a017b7f07b756171a7c
Mark Richards <mark.richards@guardian.co.uk>
2019-02-14 18:06:53 +0000

    Swap to env for vars

--- mobile-save-for-later/src/main/scala/com/gu/sfl/lambda/FetchArticlesLambda.scala
+++ mobile-save-for-later/src/main/scala/com/gu/sfl/lambda/FetchArticlesLambda.scala
@@ -1,37 +1,36 @@
 package com.gu.sfl.lambda
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClientBuilder
-import com.gu.AwsIdentity
 import com.gu.sfl.Logging
 import com.gu.sfl.controller.FetchArticlesController
 import com.gu.sfl.identity.{IdentityConfig, IdentityServiceImpl}
+import com.gu.sfl.lambda.FetchArticlesConfig.{app, stage}
 import com.gu.sfl.lib.Parallelism.largeGlobalExecutionContext
-import com.gu.sfl.lib.{CloudWatchImpl, GlobalHttpClient, SsmConfig}
+import com.gu.sfl.lib.{CloudWatchImpl, GlobalHttpClient}
 import com.gu.sfl.persistence.{PersistenceConfig, SavedArticlesPersistenceImpl}
 import com.gu.sfl.savedarticles.FetchSavedArticlesImpl
 
+object FetchArticlesConfig {
+  private val identityApihostKey = "IdentityApiHost"
+  private val stageKey = "Stage"
+  private val appKey = "App"
+
+  lazy val identityApiHost = sys.env.getOrElse(identityApihostKey, throw new NullPointerException(identityApihostKey))
+  lazy val app = sys.env.getOrElse(appKey, throw new NullPointerException(appKey))
+  lazy val stage = sys.env.getOrElse(stageKey, throw new NullPointerException(stageKey))
+}
+
 object FetchArticlesLambda extends Logging {
 
-  lazy val ssmConfig = new SsmConfig("save-for-later")
-  lazy val cloudWatchImpl = {
-    ssmConfig.identity match {
-      case awsIdentity: AwsIdentity =>
-        new CloudWatchImpl(awsIdentity.app, awsIdentity.stage, "fetch", AmazonCloudWatchAsyncClientBuilder.defaultClient())
-    }
-  }
+  lazy val cloudWatchImpl = new CloudWatchImpl(app, stage, "fetch", AmazonCloudWatchAsyncClientBuilder.defaultClient())
   lazy val savedArticledController: FetchArticlesController = logOnThrown(
     () => {
-      ssmConfig.identity match {
-        case awsIdentity: AwsIdentity =>
-          logger.debug(s"Configuring controller with environment variables: Stack: ${awsIdentity.stack} Stage: ${awsIdentity.stage} App; ${awsIdentity.app}")
-          new FetchArticlesController(
-            new FetchSavedArticlesImpl(
-              new IdentityServiceImpl(IdentityConfig(ssmConfig.config.getString("identity.apiHost")), GlobalHttpClient.defaultHttpClient),
-              new SavedArticlesPersistenceImpl(PersistenceConfig(awsIdentity.app, awsIdentity.stage), cloudWatchImpl)
-            )
-          )
-        case _ => throw new IllegalStateException("Unable to retrieve configuration")
-      }
+      new FetchArticlesController(
+        new FetchSavedArticlesImpl(
+          new IdentityServiceImpl(IdentityConfig(FetchArticlesConfig.identityApiHost), GlobalHttpClient.defaultHttpClient),
+          new SavedArticlesPersistenceImpl(PersistenceConfig(app, stage), cloudWatchImpl)
+        )
+      )
     }, "Error initialising saved articles controller")
 }
 
--- mobile-save-for-later/src/main/scala/com/gu/sfl/lambda/SaveArticlesLambda.scala
+++ mobile-save-for-later/src/main/scala/com/gu/sfl/lambda/SaveArticlesLambda.scala
@@ -1,40 +1,39 @@
 package com.gu.sfl.lambda
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClientBuilder
-import com.gu.AwsIdentity
 import com.gu.sfl.Logging
 import com.gu.sfl.controller.SaveArticlesController
 import com.gu.sfl.identity.{IdentityConfig, IdentityServiceImpl}
+import com.gu.sfl.lambda.SaveArticlesConfig.{app, stage}
 import com.gu.sfl.lib.Parallelism.largeGlobalExecutionContext
-import com.gu.sfl.lib.{CloudWatchImpl, GlobalHttpClient, SavedArticlesMergerConfig, SavedArticlesMergerImpl, SsmConfig}
+import com.gu.sfl.lib.{CloudWatchImpl, GlobalHttpClient, SavedArticlesMergerConfig, SavedArticlesMergerImpl}
 import com.gu.sfl.persistence.{PersistenceConfig, SavedArticlesPersistenceImpl}
 import com.gu.sfl.savedarticles.UpdateSavedArticlesImpl
+object SaveArticlesConfig {
+  private val identityApihostKey = "IdentityApiHost"
+  private val stageKey = "Stage"
+  private val appKey = "App"
+  private val savedArticleLimitKey = "SavedArticleLimit"
 
+  lazy val identityApiHost = sys.env.getOrElse(identityApihostKey, throw new NullPointerException(identityApihostKey))
+  lazy val app = sys.env.getOrElse(appKey, throw new NullPointerException(appKey))
+  lazy val stage = sys.env.getOrElse(stageKey, throw new NullPointerException(stageKey))
+  lazy val savedArticleLimit = sys.env.getOrElse(savedArticleLimitKey, throw new NullPointerException(savedArticleLimitKey)).toInt
+
+}
 object SaveArticlesLambda extends Logging {
-
-  lazy val ssmConfig = new SsmConfig("save-for-later")
-
-  lazy val cloudwatch = {
-    ssmConfig.identity match {
-      case awsIdentity: AwsIdentity =>
-        new CloudWatchImpl(awsIdentity.app, awsIdentity.stage, "save", AmazonCloudWatchAsyncClientBuilder.defaultClient())
-    }
-  }
+  lazy val cloudwatch = new CloudWatchImpl(app, stage, "save", AmazonCloudWatchAsyncClientBuilder.defaultClient())
 
   lazy val saveForLaterController: SaveArticlesController = logOnThrown(
     () => {
-      ssmConfig.identity match {
-        case awsIdentity: AwsIdentity =>
           new SaveArticlesController(
             new UpdateSavedArticlesImpl(
-              new IdentityServiceImpl(IdentityConfig(ssmConfig.config.getString("identity.apiHost")), GlobalHttpClient.defaultHttpClient),
-              new SavedArticlesMergerImpl(SavedArticlesMergerConfig(ssmConfig.config.getInt("savedarticle.limit")),
-                new SavedArticlesPersistenceImpl(PersistenceConfig(awsIdentity.app, awsIdentity.stage), cloudwatch)
+              new IdentityServiceImpl(IdentityConfig(SaveArticlesConfig.identityApiHost), GlobalHttpClient.defaultHttpClient),
+              new SavedArticlesMergerImpl(SavedArticlesMergerConfig(SaveArticlesConfig.savedArticleLimit),
+                new SavedArticlesPersistenceImpl(PersistenceConfig(app, stage), cloudwatch)
               )
             )
           )
-        case _ => throw new IllegalStateException("Unable to retrieve configuration")
-      }
     }, "Error initialising save for later controller")
 }
 
