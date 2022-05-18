aaaba62e92ebe40625372276170b6758f9d4e244
Jacob Winch <jacob.winch@guardian.co.uk>
2022-05-18 14:59:12 +0100

    Add cdk-defined version of API infrastructre

--- cdk/lib/mobile-save-for-later.test.ts
+++ cdk/lib/mobile-save-for-later.test.ts
@@ -1,15 +1,22 @@
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
+import { codeProps, prodProps } from "../bin/cdk";
 import { MobileSaveForLater } from "./mobile-save-for-later";
 
 describe("The MobileSaveForLater stack", () => {
   it("matches the snapshot", () => {
     const app = new App();
-    const stack = new MobileSaveForLater(app, "MobileSaveForLater", {
-      stack: "mobile",
-      stage: "TEST",
-    });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+    const codeStack = new MobileSaveForLater(
+      app,
+      "MobileSaveForLater-CODE",
+      codeProps
+    );
+    const prodStack = new MobileSaveForLater(
+      app,
+      "MobileSaveForLater-PROD",
+      prodProps
+    );
+    expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
+    expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
   });
 });
