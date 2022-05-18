eaaa10d43c9394fd1199510d1dfdac46de1e74c1
Jacob Winch <jacob.winch@guardian.co.uk>
2022-05-18 14:59:12 +0100

    Add cdk-defined version of API infrastructre

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,13 +1,29 @@
 import "source-map-support/register";
 import { App } from "aws-cdk-lib";
+import type { MobileSaveForLaterProps } from "../lib/mobile-save-for-later";
 import { MobileSaveForLater } from "../lib/mobile-save-for-later";
 
 const app = new App();
-new MobileSaveForLater(app, "MobileSaveForLater-CODE", {
+
+export const codeProps: MobileSaveForLaterProps = {
   stack: "mobile",
   stage: "CODE",
-});
-new MobileSaveForLater(app, "MobileSaveForLater-PROD", {
+  certificateId: "b4c2902a-fc80-47a9-88b7-7810b88e7e26",
+  domainName: "mobile-save-for-later.mobile-aws.code.dev-guardianapis.com",
+  hostedZoneName: "mobile-aws.code.dev-guardianapis.com",
+  hostedZoneId: "Z6PRU8YR6TQDK",
+  identityApiHost: "https://id.code.dev-guardianapis.com",
+};
+
+export const prodProps: MobileSaveForLaterProps = {
   stack: "mobile",
   stage: "PROD",
-});
+  certificateId: "0ee21f37-ec53-437c-b572-3c9d294ab749",
+  domainName: "mobile-save-for-later.mobile-aws.guardianapis.com",
+  hostedZoneName: "mobile-aws.guardianapis.com",
+  hostedZoneId: "Z1EYB4AREPXE3B",
+  identityApiHost: "https://id.guardianapis.com",
+};
+
+new MobileSaveForLater(app, "MobileSaveForLater-CODE", codeProps);
+new MobileSaveForLater(app, "MobileSaveForLater-PROD", prodProps);
