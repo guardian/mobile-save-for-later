55549e0dcc6bdf6d9601018cfe72dda06e9773d7
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2024-02-29 15:58:10 +0000

    Add logging statements

--- mobile-save-for-later-user-deletion/src/main/scala/com/gu/sfl/userdeletion/UserDeletionLambda.scala
+++ mobile-save-for-later-user-deletion/src/main/scala/com/gu/sfl/userdeletion/UserDeletionLambda.scala
@@ -10,21 +10,24 @@ import coml.gu.sfl.userdeletion.db.SflDynamoDb
 import scala.collection.JavaConverters._
 
 object UserDeletionLambda extends Logging {
-  val saveForLaterApp = Option(System.getenv("SaveForLaterApp")).getOrElse(sys.error("No main app name configured"))
-  val stage = Option(System.getenv("Stage")).getOrElse(sys.error("No main stage configured"))
+  val saveForLaterApp = Option(System.getenv("SaveForLaterApp"))
+    .getOrElse(sys.error("No main app name configured"))
+  val stage = Option(System.getenv("Stage"))
+    .getOrElse(sys.error("No main stage configured"))
 
   val sflDyanamo = new SflDynamoDb(PersistenceConfig(saveForLaterApp, stage))
 
   def handler(sQSEvent: SQSEvent) {
     val messages = sQSEvent.getRecords.asScala.map(mes => mes).toList
     logger.info(s"Recieved:  ${messages.size} mesages")
-    messages.map{
-      m =>
-        val body = m.getBody
-        logger.debug(s"Message body: ${body}")
-        val node = mapper.readTree(body)
-        val user = mapper.readValue[UserDeleteMessage](node.get("Message").asText())
-        sflDyanamo.deleteSavedArticleasForUser(user)
+    logger.info(messages)
+    messages.map { m =>
+      val body = m.getBody
+      logger.info(s"Message body: ${body}")
+      val node = mapper.readTree(body)
+      val user =
+        mapper.readValue[UserDeleteMessage](node.get("Message").asText())
+      sflDyanamo.deleteSavedArticleasForUser(user)
     }
   }
 }
