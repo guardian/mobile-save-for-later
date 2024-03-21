f601e8c8cdad35ad672f14a3aefd74b5d4278f82
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2024-03-21 15:36:42 +0000

    Update mobile user deletion

--- mobile-save-for-later-user-deletion/src/main/scala/coml/gu/sfl/userdeletion/db/SflDynamoDb.scala
+++ mobile-save-for-later-user-deletion/src/main/scala/coml/gu/sfl/userdeletion/db/SflDynamoDb.scala
@@ -4,20 +4,24 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.dynamodbv2._
 import com.amazonaws.services.dynamodbv2.model.DeleteItemResult
 import org.scanamo.{Scanamo, Table}
-import org.scanamo.auto._
 import org.scanamo.syntax._
 import com.gu.sfl.Logging
 import com.gu.sfl.persistence.{DynamoSavedArticles, PersistenceConfig}
 import com.gu.sfl.userdeletion.model.UserDeleteMessage
+import org.scanamo.DeleteReturn.OldValue
+import org.scanamo.generic.auto.genericDerivedFormat
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 
 class SflDynamoDb(persistanceConfig: PersistenceConfig) extends Logging {
 
   private val table = Table[DynamoSavedArticles](persistanceConfig.tableName)
-  private val client: AmazonDynamoDB = AmazonDynamoDBClient.builder().withCredentials(DefaultAWSCredentialsProviderChain.getInstance()).build()
+  private val client = DynamoDbClient.create()
   private val scanamo = Scanamo(client)
   def deleteSavedArticleasForUser(user: UserDeleteMessage) = {
     logger.info(s"Deleting record for user id: ${user.userId}")
-    logger.info(Option(scanamo.exec(table.delete("userId" -> user.userId))).fold(s"Unable to delete record for user ${user.userId}")((_: DeleteItemResult) => s"Deleted record for ${user.userId}") )
+    val dbResponse = scanamo.exec(table.deleteAndReturn(OldValue)("userId" === user.userId))
+      .fold(s"Unable to delete record for user ${user.userId}")((_) => s"Deleted record for ${user.userId}")
+    logger.info(dbResponse)
   }
 
 }
