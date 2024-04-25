95e5afc3054eee4384c0a354f28bf5476096f28b
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2024-04-25 11:18:49 +0100

    Use slf4j logger class

--- common/src/main/scala/com/gu/sfl/Logging.scala
+++ common/src/main/scala/com/gu/sfl/Logging.scala
@@ -1,11 +1,11 @@
 package com.gu.sfl
 
-import org.apache.logging.log4j.LogManager
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.util.{Failure, Success, Try}
 
 trait Logging {
-  val logger = LogManager.getLogger(this.getClass.getName)
+  val logger: Logger = LoggerFactory.getLogger(this.getClass.getName)
 
   def logOnThrown[T](function: () => T, messageOnError: String = ""): T = Try(function()) match {
     case Success(value) => value
